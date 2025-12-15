e2dae147ef848f482804664197a1fb3c69e0c09f
sekong11 <sekong1108@gmail.com>
2025-12-15 20:21:12 +0900

    project create

---
+++ BuyOrNot/src/test/java/com/ambiguous/buyornot/BuyOrNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambiguous.buyornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyOrNotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
