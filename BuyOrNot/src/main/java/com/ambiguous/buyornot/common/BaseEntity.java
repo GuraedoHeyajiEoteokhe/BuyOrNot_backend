ffa0f2a5465f56e91578b2d7453112f4afc5d8c6
DMXZC <ysj7240@gmail.com>
2025-12-16 12:50:40 +0900

    feat(baseEntity) : baseEntity 생성

---
+++ BuyOrNot/src/main/java/com/ambiguous/buyornot/common/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.ambiguous.buyornot.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Num;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
