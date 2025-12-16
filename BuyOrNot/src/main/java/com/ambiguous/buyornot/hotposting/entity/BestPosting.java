573e0efc1d9dc9b60ec9f9ee7da8c1d648d17aba
sekong11 <sekong1108@gmail.com>
2025-12-16 23:36:39 +0900

    feature/17/hot-entity 핫게시글과 베스트게시글 엔티티 생성

---
+++ BuyOrNot/src/main/java/com/ambiguous/buyornot/hotposting/entity/BestPosting.java
@@ -0,0 +1,32 @@
+package com.ambiguous.buyornot.hotposting.entity;
+
+import com.ambiguous.buyornot.common.BaseEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name="tbl_bestposting")
+public class BestPosting extends BaseEntity {
+
+    @Column(name = "posting_id", nullable = false, unique = true)
+    private Long postingId; // 게시글 아이디
+
+    @Column(name = "writer_id", nullable = false)
+    private Long writerId;  // 작성자 아이디
+
+    @Column(name = "symbol", nullable = false, length = 20)
+    private String symbol;  // 종목 심볼
+
+    @Column(name = "write_at", nullable = false)
+    private LocalDateTime writeAt;  // 게시글 작성 시각 (정렬 기준)
+
+    @Column(name = "registered_at", nullable = false)
+    private LocalDateTime registeredAt; // 베스트 게시글 등록 시각
+}
