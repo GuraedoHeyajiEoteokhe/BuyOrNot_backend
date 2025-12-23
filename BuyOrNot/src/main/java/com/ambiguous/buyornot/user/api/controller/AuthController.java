318dba3f3a184921699a3e058b09a11b970fc3b8
hatahng <hathang16@naver.com>
2025-12-24 02:02:54 +0900

    feat(user): JWT 기반 로그인 API 추가

--- BuyOrNot/src/main/java/com/ambiguous/buyornot/user/api/controller/AuthController.java
+++ BuyOrNot/src/main/java/com/ambiguous/buyornot/user/api/controller/AuthController.java
@@ -1,7 +1,13 @@
 package com.ambiguous.buyornot.user.api.controller;
 
+import com.ambiguous.buyornot.common.support.response.ApiResult;
+import com.ambiguous.buyornot.user.api.controller.request.LoginRequest;
+import com.ambiguous.buyornot.user.api.controller.response.TokenResponse;
+import com.ambiguous.buyornot.user.api.domain.AuthService;
+import com.ambiguous.buyornot.user.api.storage.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+    private final UserRepository userRepository;
 
+    @PostMapping("/login")
+    public ApiResult<?> login (@RequestBody LoginRequest request){
+        TokenResponse token = authService.login(request);
+        return ApiResult.success(token);
+    }
 }
