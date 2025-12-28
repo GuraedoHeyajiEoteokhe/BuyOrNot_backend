9498d86aee742201aedadf0028fc061ebdc7ad4b
hatahng <hathang16@naver.com>
2025-12-28 22:02:09 +0900

    feat(user): 회원 탈퇴(users/me) API 추가

--- BuyOrNot/src/main/java/com/ambiguous/buyornot/user/api/controller/UserCommandController.java
+++ BuyOrNot/src/main/java/com/ambiguous/buyornot/user/api/controller/UserCommandController.java
@@ -2,10 +2,10 @@ package com.ambiguous.buyornot.user.api.controller;
 
 import com.ambiguous.buyornot.common.support.response.ApiResult;
 import com.ambiguous.buyornot.user.api.controller.request.UserCreateRequest;
+import com.ambiguous.buyornot.user.api.controller.request.UserWithdrawRequest;
 import com.ambiguous.buyornot.user.api.domain.UserCommandService;
-import io.swagger.v3.oas.models.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +19,12 @@ public class UserCommandController {
     @PostMapping("/regist")
     public ApiResult<?> registerUser (@RequestBody UserCreateRequest request){
         userCommandService.registerUser(request);
+        return ApiResult.success();
+    }
+
+    @DeleteMapping("/users/me")
+    public ApiResult<?> withdrawUser (@RequestBody UserWithdrawRequest request){
+        userCommandService.withdrawUser(request);
         return ApiResult.success();
     }
 
