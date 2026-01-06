8dc5c7e899abfa9c8233b41f4661ee214179b6fd
sekong11 <sekong1108@gmail.com>
2026-01-06 21:06:05 +0900

    feat(bestposting): 베스트 포스팅 등록

--- BuyOrNot/src/main/java/com/ambiguous/buyornot/posting/api/domain/PostReactionService.java
+++ BuyOrNot/src/main/java/com/ambiguous/buyornot/posting/api/domain/PostReactionService.java
@@ -1,8 +1,11 @@
 package com.ambiguous.buyornot.posting.api.domain;
 
+import com.ambiguous.buyornot.posting.storage.BestPostingRepository;
 import com.ambiguous.buyornot.posting.storage.PostReactionRepository;
 import com.ambiguous.buyornot.posting.storage.PostRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,11 +13,14 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class PostReactionService {
+    private static final long BEST_THRESHOLD = 100;
 
     private final PostRepository postRepository;
     private final PostReactionRepository postReactionRepository;
+    private final BestPostingRepository bestPostingRepository;
 
     public void react(Long postId, Long userId, ReactionType type) {
         Post post = postRepository.findById(postId)
@@ -27,6 +33,11 @@ public class PostReactionService {
         if (optional.isEmpty()) {
             postReactionRepository.save(new PostReaction(postId, userId, type));
             post.increaseReaction(type);
+
+            if (type == ReactionType.LIKE) {
+                tryRegisterBestPost(postId, post.getStockId(), post.getLikeCount());
+            }
+
             return;
         }
 
@@ -38,10 +49,29 @@ public class PostReactionService {
             postReactionRepository.delete(reaction);
             return;
         }
+        ReactionType prev = reaction.getType();
 
         // 반응 변경
-        post.decreaseReaction(reaction.getType());
+        post.decreaseReaction(prev);
         reaction.changeType(type);
         post.increaseReaction(type);
+
+        if (prev != ReactionType.LIKE && type == ReactionType.LIKE) {
+            tryRegisterBestPost(postId, post.getStockId(), post.getLikeCount());
+        }
+    }
+
+    // 베스트 포스트 등록
+    private void tryRegisterBestPost(Long postId, Long stockId, long likeCount){
+        if (likeCount < BEST_THRESHOLD) return;
+
+        try {
+            bestPostingRepository.save(BestPosting.builder()
+                    .postingId(postId)
+                    .stockId(stockId)
+                    .build());
+        } catch (DataIntegrityViolationException e) {
+            log.debug("BestPosting already exists. postId={}", postId);
+        }
     }
 }
